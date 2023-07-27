c2965d048489a8d966b1cf6822cfac56833365cf
rndyhu <93273986+rndyhu@users.noreply.github.com>
2023-07-27 23:19:37 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'ondyhu-rndyhu.b4a.run'
+      url.hostname = 'www.hicairo.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
